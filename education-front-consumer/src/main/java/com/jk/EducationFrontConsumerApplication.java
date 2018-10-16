7eff960f8c3f1843053ce0c2cd9876488f08b8ce
zhangzjGIT <1094737153@qq.com>
2018-10-16 15:54:51 +0800

    第一次提交前台框架provider+consumer

---
+++ education-front-consumer/src/main/java/com/jk/EducationFrontConsumerApplication.java
@@ -0,0 +1,18 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients //注解开启Feign的功能
+public class EducationFrontConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationFrontConsumerApplication.class, args);
+    }
+}
