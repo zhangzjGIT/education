54f997cdfc43945beb292741815d0bf00616b6c7
“Ldwei <524121642@qq.com>
2018-10-22 09:19:19 +0800

    ldw10.22

--- education-back-consumer/src/main/java/com/jk/EducationBackConsumerApplication.java
+++ education-back-consumer/src/main/java/com/jk/EducationBackConsumerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
@@ -13,6 +15,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class EducationBackConsumerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(EducationBackConsumerApplication.class, args);
     }
+
+
 }
