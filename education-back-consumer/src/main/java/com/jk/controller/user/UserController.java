15aafd321377fa7b91c86db407b7b2a06387a9e4
“Ldwei <524121642@qq.com>
2018-10-22 13:31:39 +0800

    55522

--- education-back-consumer/src/main/java/com/jk/controller/user/UserController.java
+++ education-back-consumer/src/main/java/com/jk/controller/user/UserController.java
@@ -189,6 +189,9 @@ public class UserController {
         System.out.println("李达伟");
         System.out.println("李达伟");
         System.out.println("李达伟");
+        System.out.println("李达伟");
+
+
         return "teacher/loginInfo";
     }
 
